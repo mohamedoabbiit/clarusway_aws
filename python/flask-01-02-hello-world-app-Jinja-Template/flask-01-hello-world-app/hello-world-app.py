a0d8f0db208f52682a540ffd82c7c5794a9a2e9a
mohammedoabbiit <mohamedoabbiit@gmail.com>
2021-06-16 18:02:38 -0400

    Loadind first flask app

---
+++ python/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+
+app= Flask(__name__)
+@app.route('/')
+
+def hello() :
+    return "hello world"
+
+@app.route('/second')
+
+def second():
+    return "This is second page"    
+
+@app.route('/third/subthird')
+
+def third():
+    return "This is subpage of third page"
+
+if __name__=="__main__":
+    app.run(debug=True)
